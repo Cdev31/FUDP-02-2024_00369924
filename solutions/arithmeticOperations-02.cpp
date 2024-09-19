c3411db67b616f48fdfa375881d2ec4963ef4bb1
kalet Chavez <kalet.svdev@gmail.com>
2024-09-18 22:56:42 -0600

    :sparkles: add arithmeticOperations-02 and implicit convertion division result in float

---
+++ solutions/arithmeticOperations-02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1 = 23, number2 = 28;
+
+    //its making process and print sum result
+    cout << "your sum are: " << number1 + number2 << endl;
+
+    //its making process and print remainder result
+    cout << "your remainder are: " << number1 - number2 << endl;
+
+    //its making process and print multiplication result
+    cout << "your multiplication are: " << number1 * number2 << endl;
+
+    //its making process and print division result
+    cout << "your division are: " << (float)number1 / (float)number2 << endl;
+    
+    return 0;
+}
