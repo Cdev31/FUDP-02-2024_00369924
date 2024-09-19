26bfe53eb4b9234ef7e96fc2156ac06dc0728641
kalet Chavez <kalet.svdev@gmail.com>
2024-09-18 23:44:33 -0600

    :recycle: choose other code

--- solutions/averageOperation-04.cpp
+++ solutions/averageOperation-04.cpp
@@ -2,27 +2,59 @@
 
 using namespace std;
 
+// int main()
+// {
+//     //variable declaration
+//     float gradeOne, gradeTwo, gradeThree;
+
+//     //it give first grade
+//     cout << "ingrese su primera nota: " << endl;
+//     cin >> gradeOne;
+
+//     //it give second grade
+//     cout << "ingrese su segunda nota: " << endl;
+//     cin >> gradeTwo;
+
+//     //it give thrid grade
+//     cout << "ingrese su tercera nota: " << endl;
+//     cin >> gradeThree;
+
+//     //process
+//     float average = ( gradeOne + gradeTwo + gradeThree) / 3;
+
+//     cout << "Su promedio es: " << average << endl;
+
+//     return 0;
+// }
+
+//other away
 int main()
 {
     //variable declaration
-    float gradeOne, gradeTwo, gradeThree;
+    float sumGrades;
 
-    //it give first grade
-    cout << "ingrese su primera nota: " << endl;
-    cin >> gradeOne;
+    int counter = 0;
 
-    //it give second grade
-    cout << "ingrese su segunda nota: " << endl;
-    cin >> gradeTwo;
+    while ( counter < 3){
 
-    //it give thrid grade
-    cout << "ingrese su tercera nota: " << endl;
-    cin >> gradeThree;
+        float values;
+
+        //it give values
+        cout << "Ingrese su nota " << ( counter + 1 ) << ":" << endl;
+        cin >> values ;
+
+        // sum grades
+        sumGrades = sumGrades + values;
+
+        counter++;
+    }
 
     //process
-    float average = ( gradeOne + gradeTwo + gradeThree) / 3;
+    float average = sumGrades / 3;
 
     cout << "Su promedio es: " << average << endl;
 
     return 0;
 }
+
+
