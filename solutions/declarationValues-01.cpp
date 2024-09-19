b15da5fc34b185430f8835d5672cdeffe40ad80e
kalet Chavez <kalet.svdev@gmail.com>
2024-09-18 22:46:22 -0600

    :sparkles: add file .gitignore and add declaratioValues

---
+++ solutions/declarationValues-01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // variable declaration
+    int numberOne = 10, numberTwo = 11, numberThree = 12;
+    float decimalOne = 10.25f, decimalTwo = 10.35f, decimalThree = 10.45f;
+    char oneChar = 'a', twoChar ='b', threChar ='c';
+
+    // print integer variables
+    cout << "The first integer value is: " << numberOne << endl;
+    cout << "The second integer value is: " << numberTwo << endl;
+    cout << "The thrid integer value is: " << numberThree << endl;
+
+    // print decimal variables
+    cout << "The first decimal value is: " << decimalOne << endl;
+    cout << "The second decimal value is: " << decimalTwo << endl;
+    cout << "The thrid decimal value is: " << decimalThree << endl;
+
+    // print char variables
+    cout << "The first char value is: " << oneChar << endl;
+    cout << "The second char value is: " << twoChar << endl;
+    cout << "The thrid char value is: " << threChar << endl;
+    
+    return 0;
+};
