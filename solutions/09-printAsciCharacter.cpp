6605650cd795caee572f314acfc1aa10617c9a1c
kalet Chavez <kalet.svdev@gmail.com>
2024-09-19 20:56:10 -0600

    :sparkles: add printAsciiCharacter file

---
+++ solutions/09-printAsciCharacter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char charValue ;
+
+    cout << "ingrese su caracter:" << endl;
+    cin >> charValue;
+
+    cout << "El codigo ASCII de el caracter " << charValue << " es: " << (int)charValue << endl;
+
+    return 0;
+}
